35c97b10932aa2a33c90ec4f56d668613a6d7b08
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-04-03 14:11:10 +0500

    Class Basics: Added -> scope & shadowing

--- app/src/main/java/com/example/kotlinbasics/Class_Basics.kt
+++ app/src/main/java/com/example/kotlinbasics/Class_Basics.kt
@@ -1,12 +1,21 @@
 package com.example.kotlinbasics
-
+//var b = 3
 fun main(){
     var denis = Person("Denis", "Panjuta")
     var john = Person()
     var ohnPeterson = Person(lastName = "Peterson")
+    myFunction(5)
+    //b = 5
 }
 class Person(firstName: String = "John", lastName: String = "Doe"){
     init {
         println("Initialized a new Person object with firstName = $firstName and lastName = $lastName")
     }
+}
+//Scope and Shadowing
+// this a is a parameter
+fun myFunction(a:Int){
+    // a is a variable
+    var b = 4
+    println("a is $b")
 }
