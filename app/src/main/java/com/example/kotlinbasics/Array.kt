835b9ea6664173fd24b411a1d8ed61e142994a5d
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-04-04 12:14:09 +0500

    Array: Added -> array list code

--- app/src/main/java/com/example/kotlinbasics/Array.kt
+++ app/src/main/java/com/example/kotlinbasics/Array.kt
@@ -42,6 +42,33 @@ fun main(){
 
     val mix = arrayOf("Sun", "Mon", "Tues",1, 2, 3, Fruit("Apple", 2.5))
     print(mix.contentToString())
+
+
+    // Array list
+        val myArrayList: ArrayList<Double> = ArrayList()
+        myArrayList.add(13.212312)
+        myArrayList.add(23.151232)
+        myArrayList.add(32.651553)
+        myArrayList.add(16.223817)
+        myArrayList.add(18.523999)
+        var total = 0.0
+        for (i in myArrayList){
+            total += i
+        }
+        var average = total / myArrayList.size
+        println("\nAverage is $average")
+
+    // Array list using collection
+    val arrayList : ArrayList<String> = ArrayList<String>(5)
+    var list:MutableList<String> = mutableListOf<String>()
+    list.add("One")
+    list.add("Two")
+    arrayList.addAll(list)
+    val itr =arrayList.iterator()
+    while (itr.hasNext()){
+        print("\n${itr.next()}\n")
+    }
+    println("size of arrayList ${arrayList.size}")
 }
 
 data class Fruit(val name: String,val price: Double)
